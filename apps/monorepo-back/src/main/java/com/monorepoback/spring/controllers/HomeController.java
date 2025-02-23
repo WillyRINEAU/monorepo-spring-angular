0b2e64983079e283cd15ef1245e5344e253addaf
Willy RINEAU <willy.rineau@syd.Fr>
2025-02-23 17:06:34 +0100

    chore(backend): added Grafana / Prometheus support - Created first backend controller

---
+++ apps/monorepo-back/src/main/java/com/monorepoback/spring/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.monorepoback.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from monorepo-back!";
+    }
+}
